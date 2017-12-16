8c40f706a6f6ca0ae7984b38d20d0ee10fdd8acf
jfbernal92 <jota.efebr@hotmail.com>
2017-12-16 11:03:09 +0100

    Array

--- array.rb
+++ array.rb
@@ -1,7 +1,26 @@
-numeros = Array.new(20)
-for i in 0..numeros.length
-	numeros[i] = rand(100)
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+require 'prime'
+
+numeros = []
+for i in 0...20
+	numeros[i] = rand(100) 
 end
-for i in 0..numeros.length
-	puts numeros[i]
-end
+puts "Array generado:"
+p numeros
+puts "Array ordenado:"
+aux = numeros.clone
+numeros.clear
+cont = 0;
+aux.each{ |n| 
+
+  if n.prime? 
+    numeros.insert(cont, n)
+    cont += 1
+  else
+    numeros.push(n)
+  end
+}
+p (numeros)
+
